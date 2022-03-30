a32895db781a8b4885fb47155a6094455b767ca8
Roman Budnikov <romanyx90@yandex.ru>
2022-03-30 23:25:22 +0300

    1.18 version changes

---
+++ README.md
@@ -0,0 +1,32 @@
+# Dependency injection tool
+
+[![GoDoc](https://godoc.org/github.com/romanyx/shelf?status.svg)](https://godoc.org/github.com/romanyx/shelf)
+
+Shelf allows to easily access dependencies across codebase.
+
+* **Example**.
+
+``` go
+func main() {
+  shelf.Put[*greeter](&greeter{w: "Hello"}) 
+  shelf.Put[*greeter](&greeter{w: "こんにちは"}, "jp") 
+
+  default := shelf.Take[*greeter]()
+  default.Hello()
+  jp := shelf.Take[*greeter]("jp")
+  jp.Hello()
+
+  //
+  // Hello
+  // こんにちは
+}
+
+type greeter struct{
+  w string
+}
+
+func(g greeter) Hello() {
+  fmt.Println(g.w)
+}
+```
+
--- shelf.go
+++ shelf.go
@@ -10,30 +10,34 @@ var (
 	shelf sync.Map
 )
 
-type dep struct {
+type dep[T any] struct {
 	s *stack
-	d interface{}
+	d T
 }
 
-// Put allows to put dependency on shelf
-func Put(name string, d interface{}) {
-	shelf.Store(name, dep{
+// Put allows to put dependency on shelf with specific name
+// or using type as a key.
+func Put[T any](d T, name ...string) {
+	key := getKey[T](name...)
+	shelf.Store(key, dep[T]{
 		d: d,
 		s: callers(),
 	})
 }
 
-// Take allows to take dependency from shelf.
-func Take(name string) interface{} {
-	d, ok := shelf.Load(name)
+// Take allows to take dependency from shelf by
+// type or specific name.
+func Take[T any](name ...string) interface{} {
+	key := getKey[T](name...)
+	d, ok := shelf.Load(key)
 	if !ok {
-		panic(fmt.Sprintf("dependency %s does not exists", name))
+		panic(fmt.Sprintf("dependency %s does not exists", key))
 	}
 
-	dep := d.(dep) // nolint: errcheck
+	dep := d.(dep[T]) // nolint: errcheck
 
 	if isNil(dep.d) {
-		m := fmt.Sprintf("dependency %s holds nil value\n", name)
+		m := fmt.Sprintf("dependency %s holds nil value\n", key)
 		for _, frame := range dep.s.StackTrace() {
 			m += fmt.Sprintf(
 				"%s:%d\n",
@@ -45,6 +49,23 @@ func Take(name string) interface{} {
 	}
 
 	return dep.d
+}
+
+func getKey[T any](name ...string) string {
+	if len(name) > 1 {
+		panic("only one key allowed")
+	}
+
+	var key string
+	if len(name) == 1 {
+		key = name[0]
+	}
+	if key == "" {
+		v := new(T)
+		key = fmt.Sprintf("%s", reflect.TypeOf(*v))
+	}
+
+	return key
 }
 
 func isNil(i interface{}) bool {
